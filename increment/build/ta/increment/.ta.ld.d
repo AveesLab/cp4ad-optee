0cb53e0399a78d4557b6780640513e6fce7ddda6
SeungWoo3 <rohseungwoo@gmail.com>
2025-02-19 23:05:13 +0900

    add tag to folders

--- increment/build/ta/increment/.ta.ld.d
+++
@@ -1,2 +0,0 @@
-../build/ta/increment/ta.lds: \
- /home/seungwoo/optee/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/src/ta.ld.S
--- secret_text/ta/sub.mk
+++
@@ -1,2 +0,0 @@
-## 1. Modify srcs-y to the corresponding source files
-srcs-y += ta_secret_text.c
