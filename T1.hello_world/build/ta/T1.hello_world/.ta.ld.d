3ab9dc4d46f13709e0c184792e536f2967d249cc
SeungWoo3 <shtmddn49@kookmin.ac.kr>
2025-02-25 11:10:58 +0900

    changes in T1.hello_world/Makefile
    - abs path to relative path
    - add MAKEFLAGS
          set -e
          echo msg in the end

---
+++ T1.hello_world/build/ta/T1.hello_world/.ta.ld.d
@@ -0,0 +1,2 @@
+../build/ta/T1.hello_world/ta.lds: \
+ /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/src/ta.ld.S
---
+++ T1.hello_world/build/ta/T1.hello_world/.ta_hello_world.o.d
@@ -0,0 +1,18 @@
+../build/ta/T1.hello_world/ta_hello_world.o: ta_hello_world.c \
+ /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/include/tee_internal_api.h \
+ /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/include/compiler.h \
+ /usr/lib/gcc/aarch64-linux-gnu/9/include/stddef.h \
+ /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/include/tee_api_defines.h \
+ /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/include/tee_api_types.h \
+ /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/include/inttypes.h \
+ /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/include/stdint.h \
+ /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/include/limits.h \
+ /usr/lib/gcc/aarch64-linux-gnu/9/include/stdbool.h \
+ /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/include/trace.h \
+ /usr/lib/gcc/aarch64-linux-gnu/9/include/stdarg.h \
+ /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/include/trace_levels.h \
+ /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/include/tee_api_compat.h \
+ /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/include/tee_internal_api_extensions.h \
+ /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/include/stdio.h \
+ /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/include/tee_api_defines_extensions.h \
+ ta_hello_world.h
