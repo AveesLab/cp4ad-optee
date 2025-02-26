6be9c8d42f773417c2248d08e4ccf03d1f23adf7
SeungWoo3 <shtmddn49@kookmin.ac.kr>
2025-02-26 15:52:24 +0900

    build/ -> gitignore

--- T5.optee_crypto/build/ta/T5.optee_crypto/.ta.ld.d
+++
@@ -1,2 +0,0 @@
-/home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/cp4ad-optee/T5.optee_crypto/build/ta/T5.optee_crypto/ta.lds: \
- /home/avees/optee_local_build/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/src/ta.ld.S
---
+++ T6.RSA/ta/ta_rsa.h
@@ -0,0 +1,13 @@
+// 1. Modify macro variable name(header guard), if needed.
+#ifndef TA_RSA_H
+#define TA_RSA_H
+
+// 2. Modify UUID name and UUID.
+#define TA_RSA_UUID \
+	{ 0xe64ec340, 0xed6e, 0x4dce, \
+		{ 0xaa, 0x40, 0xe5, 0xd3, 0xe0, 0xe1, 0xc8, 0xfa} }
+
+// 3. Modify command ID.
+#define CMD_RSA 0
+
+#endif
