65fbf4bcc0df0f6cd96823023d3f9d09f59e26f3
SeungWoo3 <shtmddn49@kookmin.ac.kr>
2025-05-02 19:56:36 +0900

    change script to be executed without sudo option

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
+sudo apt install pip python3-pyelftools device-tree-compiler -y
+
 cd /home/avees
-wget https://developer.nvidia.com/downloads/embedded/l4t/r35_release_v6.0/release/jetson_linux_r35.6.0_aarch64.tbz2
+# wget https://developer.nvidia.com/downloads/embedded/l4t/r35_release_v6.0/release/jetson_linux_r35.6.0_aarch64.tbz2
 sudo tar -xvf jetson_linux_r35.6.0_aarch64.tbz2
 /home/avees/Linux_for_Tegra/source_sync.sh -t jetson_35.6
 
 echo "export CROSS_COMPILE_AARCH64_PATH=/usr" >> /home/avees/.bashrc
 echo "export CROSS_COMPILE_AARCH64=${CROSS_COMPILE_AARCH64_PATH}/bin/" >> /home/avees/.bashrc
 echo "export UEFI_STMM_PATH=/home/avees/Linux_for_Tegra/bootloader/standalonemm_optee_t234.bin" >> /home/avees/.bashrc
-
-echo "export CROSS_COMPILE_AARCH64_PATH=/usr" >> /home/avees/.bashrc
-echo "export CROSS_COMPILE_AARCH64=${CROSS_COMPILE_AARCH64_PATH}/bin/" >> /root/.bashrc
-echo "export UEFI_STMM_PATH=/home/avees/Linux_for_Tegra/bootloader/standalonemm_optee_t234.bin" >> /root/.bashrc
 
 export CROSS_COMPILE_AARCH64_PATH=/usr
 export CROSS_COMPILE_AARCH64=${CROSS_COMPILE_AARCH64_PATH}/bin/
@@ -22,15 +20,12 @@ CROSS_COMPILE="${CROSS_COMPILE_AARCH64}" \
 DEBUG=0 LOG_LEVEL=20 PLAT=tegra SPD=opteed TARGET_SOC=t234 V=0
 
 echo "!!!!!!!!!!!  build optee  !!!!!!!!!!!!"
-sudo apt install pip -y
 pip install --upgrade cryptography -y
 cd /home/avees/Linux_for_Tegra/sources/tegra/optee-src/nv-optee
-sudo apt install python3-pyelftools
 /home/avees/Linux_for_Tegra/sources/tegra/optee-src/nv-optee//optee_src_build.sh -p t234
 
 echo "!!!!!!!!!!!  dtc  !!!!!!!!!!!!"
 cd /home/avees/Linux_for_Tegra/sources/tegra/optee-src/nv-optee
-sudo apt install device-tree-compiler -y
 dtc -I dts -O dtb -o ./optee/tegra234-optee.dtb ./optee/tegra234-optee.dts
 
 cd /home/avees/Linux_for_Tegra/sources/tegra/optee-src
