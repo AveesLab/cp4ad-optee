9b614cb1d8f9c2e3ac24cbdbde02a20f64ddeca6
SeungWoo3 <shtmddn49@kookmin.ac.kr>
2025-05-09 18:56:07 +0900

    chage name from 06.string to -7.secret_text

---
+++ A7.secret_text/ta/ta.c
@@ -0,0 +1,42 @@
+#include <tee_internal_api.h>
+#include <tee_internal_api_extensions.h>
+#include "ta.h"
+#include <string.h>
+
+static char secure_data[100];
+
+TEE_Result TA_CreateEntryPoint(void) {
+    return TEE_SUCCESS;
+}
+
+void TA_DestroyEntryPoint(void) {
+}
+
+TEE_Result TA_OpenSessionEntryPoint(uint32_t param_types, TEE_Param params[], void **sess_ctx) {
+    (void)param_types;
+    (void)params;
+    (void)sess_ctx;
+    return TEE_SUCCESS;
+}
+
+void TA_CloseSessionEntryPoint(void *sess_ctx) {
+    (void)sess_ctx;
+}
+
+TEE_Result TA_InvokeCommandEntryPoint(void *sess_ctx, uint32_t cmd_id, uint32_t param_types, TEE_Param params[]) {
+    (void)sess_ctx;
+    (void)param_types;
+    strcpy(secure_data, "[This is a secret text.]"); 
+
+    switch (cmd_id) {
+    case CMD_SESSION_ACCESS:
+        TEE_MemMove(params[0].memref.buffer, secure_data, strlen(secure_data) + 1);
+        return TEE_SUCCESS;
+
+    case CMD_DIRECT_ACCESS:
+        params[0].value.a = (uintptr_t)secure_data;
+        return TEE_SUCCESS;
+    default:
+        return TEE_ERROR_BAD_PARAMETERS;
+    }
+}
