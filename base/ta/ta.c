6489026a2cc1d949fc380db5f8d89907aafdbe75
SeungWoo3 <shtmddn49@kookmin.ac.kr>
2025-05-09 18:52:19 +0900

    add 06 and A6

--- base/ta/ta.c
+++ base/ta/ta.c
@@ -10,7 +10,7 @@ void TA_DestroyEntryPoint(void) {
 }
 
 TEE_Result TA_OpenSessionEntryPoint(uint32_t param_types, 
-                                TEE_Param params[4], void **sess_ctx) {
+                                TEE_Param params[], void **sess_ctx) {
     (void)param_types;
     (void)params;
     (void)sess_ctx;
@@ -22,7 +22,7 @@ void TA_CloseSessionEntryPoint(void *sess_ctx) {
 }
 
 TEE_Result TA_InvokeCommandEntryPoint(void *sess_ctx, uint32_t cmd_id, 
-                                uint32_t param_types, TEE_Param params[4]) {
+                                uint32_t param_types, TEE_Param params[]) {
     (void)sess_ctx;
     (void)param_types;
     (void)params;
