3aca28e166ab231b09bfe85664840efdb2cfee7d
SeungWoo3 <shtmddn49@kookmin.ac.kr>
2025-05-19 12:01:50 +0900

    07.string done

---
+++ 07.string/ta/ta.h
@@ -0,0 +1,7 @@
+#ifndef TA_H
+#define TA_H
+
+#define CMD_STRING 0
+#define CMD_ECHO 1
+
+#endif
---
+++ A7.string/ta/user_ta_header_defines.h
@@ -0,0 +1,19 @@
+ #ifndef USER_TA_HEADER_DEFINES_H
+ #define USER_TA_HEADER_DEFINES_H
+ 
+ #include <ta.h>
+ #define TA_UUID				{0x54eac490, 0x4f7a, 0x4db5, {0x8f, 0xd7, 0x22, 0x1b, 0x7a, 0x22, 0x39, 0xea}}
+ #define TA_FLAGS			TA_FLAG_EXEC_DDR
+ #define TA_STACK_SIZE			(2 * 1024)
+ #define TA_DATA_SIZE			(32 * 1024)
+ #define TA_VERSION	"1.0"
+ #define TA_DESCRIPTION	"Example of OP-TEE Hello World Trusted Application"
+ #define TA_CURRENT_TA_EXT_PROPERTIES \
+     { "org.linaro.optee.examples.hello_world.property1", \
+     USER_TA_PROP_TYPE_STRING, \
+         "Some string" }, \
+     { "org.linaro.optee.examples.hello_world.property2", \
+     USER_TA_PROP_TYPE_U32, &(const uint32_t){ 0x0010 } }
+ 
+ #endif /* USER_TA_HEADER_DEFINES_H */
+ 
